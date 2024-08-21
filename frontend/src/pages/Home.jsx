3e302d7b804f2b8bca89dd86d305661870891ea1
karamat Ali <maqpoon4@gmail.com>
2024-08-21 16:13:16 +0500

    Home page changes

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,8 +1,32 @@
-import React from 'react'
+import React from "react";
+import "../App.css";
+import { Link } from "react-router-dom";
+
 
 function Home() {
   return (
-    <div>Home</div>
+    <>
+    <div className="landing">
+          <div className="home-wrap">
+            <div className="home-inner">
+              <div className="home-bg"></div>
+            </div>
+          </div>
+        </div>
+        <div className="caption text-center" style={{ textAlign: "center" }}>
+          <h1>
+            Welcome to  <br />
+            web-based attendance management system
+          </h1>
+          <Link
+            className="btn btn-outline-light btn-lg get_started"
+            to="/login"
+          >
+            Get Started
+          </Link>
+        </div>
+      
+    </>
   )
 }
 
