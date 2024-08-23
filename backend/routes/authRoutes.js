fa75f2bd9311fc7a64c1fcf7e900e96f45ba0ade
karamat Ali <maqpoon4@gmail.com>
2024-08-23 12:21:35 +0500

    changes routes

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -17,11 +17,9 @@ const storage = multer.diskStorage({
 const upload = multer({ storage: storage });
 
 // Register route
-router.get("/register", authControllers.register); // GET method (though typically unnecessary for register)
 router.post("/register", upload.single('profilePic'), authControllers.register); // POST method
 
 // Login route
-router.get("/login", authControllers.login); // GET method (again, usually not needed for login)
 router.post("/login", authControllers.login); // POST method
 
 module.exports = router;
